185db3940a840d22171967c3b79cc3d50c7ffa90
abenteuerzeit <amroz.muzyka@gmail.com>
2023-05-25 00:54:29 +0200

    Add configuration files for development environment and update dependencies
    - Add `config/default.json` with `"env": "development"` for development environment configuration.
    - Update `index.js` to use `config` module and log server startup for development environment.
    - Add `"config": "^3.3.9"`, `"errorhandler": "^1.5.1"`, and `"morgan": "^1.10.0"` packages to `dependencies` in `package.json`.
    - Update `"nodemon": "^2.0.22"` package to devDependencies in `package.json`.
    - Minor refactoring in `index.js`.

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
-const modelsPath = path.join(__dirname, '_openai', 'models.json');
-const { formatDataToTableView } = require('./helper');
+const config = require("config");
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+const bodyParser = require("body-parser");
+const { formatDataToTableView } = require("./helper");
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json())
+if (config.get("env") !== "production") {
+    const morgan = require("morgan");
+    const errorHandler = require("errorhandler");
+    app.use(morgan("dev"));
+    app.use(errorHandler());
+}
 
-app.get('*', (req, res) => {
-    let jsonData = JSON.parse(fs.readFileSync(modelsPath, "utf8")).data;
-    const formattedTable = formatDataToTableView(jsonData);
-    return res.status(200).send(formattedTable);
+app.use(bodyParser.json());
+
+const modelsPath = path.resolve(__dirname, "_openai", "models.json");
+
+app.get("*", async (req, res) => {
+    try {
+        const jsonData = JSON.parse(await fs.readFile(modelsPath, "utf8")).data;
+        const formattedTable = formatDataToTableView(jsonData);
+        res.status(200).send(formattedTable);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(`
+      <h1>Internal Server Error</h1>
+      <p>Could not find models.json file. Please make sure you have a models.json file in the _openai folder.</p>
+      <p>${error}</p>
+    `);
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
-
-
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
